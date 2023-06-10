63f3db190e59d306c678e7ddf0a823635920dfaa
DeXon <DeXon18@gmail.com>
2023-06-10 19:20:55 +0200

    Update Remove-mkv-titles.bat
    Verificar si mkvpropedit.exe existe

--- Remove-mkv-titles.bat
+++ Remove-mkv-titles.bat
@@ -1,8 +1,18 @@
 @echo off
+set "MKVToolNix=C:\Program Files\MKVToolNix\mkvpropedit.exe"
+
+REM Verificar si mkvpropedit.exe existe en la ubicación especificada
+if not exist "%MKVToolNix%" (
+    echo El archivo mkvpropedit.exe no se encontró en la ubicación especificada.
+    echo Descargue MKVToolNix desde el siguiente enlace:
+    echo https://mkvtoolnix.download/downloads.html
+    exit /b
+)
+
 REM Busca recursivamente todos los archivos con extensión .mkv en el directorio actual y subdirectorios
 for /r %%i in (*.mkv) do (
     REM Imprime un mensaje indicando el archivo que se está procesando actualmente
-    echo processing "%%i"
+    echo Processing "%%i"
     REM Ejecuta el comando mkvpropedit.exe de la herramienta MKVToolNix para eliminar el título del archivo
-    "C:\Program Files\MKVToolNix\mkvpropedit.exe" "%%i" -d title
+    "%MKVToolNix%" "%%i" -d title
 )
